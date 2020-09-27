733783ce86882427df634de062b671cab21383d9
hamisicodes <newhamisi@gmail.com>
2020-09-27 21:16:55 +0300

    work on the user cart and rendering totals

--- store/models.py
+++ store/models.py
@@ -33,14 +33,31 @@ class Order(models.Model):
     complete = models.BooleanField(default=False)
     transaction_id = models.CharField(max_length=100 , null=True)
 
+    @property
+    def get_cart_total(self):
+        orderitems =  self.orderitem_set.all()
+        total = sum([item.get_total for item in orderitems])
+        return total
+
+    @property
+    def get_cart_items(self):
+        orderitems = self.orderitem_set.all()
+        total = sum([item.quantity for item in orderitems])
+        return total
+
     def __str__(self):
         return str(self.id)
 
 class OrderItem(models.Model):
-    customer = models.ForeignKey(Customer , on_delete=models.SET_NULL , null=True )
+    product = models.ForeignKey(Product , on_delete=models.SET_NULL , null=True )
     order = models.ForeignKey(Order , on_delete=models.SET_NULL , null=True )
     quantity = models.IntegerField(default=0 , blank=True , null=True)
     date_added = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def get_total(self):
+        total = self.product.price * self.quantity
+        return total
 
   
 
